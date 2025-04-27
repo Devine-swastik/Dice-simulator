7ae32d1bea3ce6f902b24cb2a201db3725cbc8af
Swastik Kesharwani <swastikkesharwani029@gmail.com>
2025-04-27 20:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def dice():
-    dice_num = random.randint(1, 6)
-    return render_template('index.html', dice=dice_num)
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
