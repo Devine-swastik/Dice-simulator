a277365d50da864e6ab56cf57c153c2e713337e4
Swastik Kesharwani <swastikkesharwani029@gmail.com>
2025-04-27 20:51:16 +0530

    Delete templates/script.js

--- templates/script.js
+++
@@ -1,8 +0,0 @@
-// Function to roll the dice and update the value
-function rollDice() {
-    const diceValue = Math.floor(Math.random() * 6) + 1; // Random number between 1-6
-    document.getElementById('diceValue').textContent = diceValue;
-}
-
-// Attach event listener to the button
-document.getElementById('rollDice').addEventListener('click', rollDice);
